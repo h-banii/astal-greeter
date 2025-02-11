52231ab41c7dcec5bc40ab2bcdc1e79020420440
h-banii <121690516+h-banii@users.noreply.github.com>
2025-02-11 20:01:26 -0300

    style: Catch exec errors to avoid crash

--- src/style/index.ts
+++ src/style/index.ts
@@ -1,23 +1,32 @@
-import { exec, GLib } from "astal";
+import { execAsync, GLib } from "astal";
 import State from "../state";
+import colors_fallback from "./colors.json";
 
 const static_path = `${SRC}/src/style`;
 const dynamic_path = `/tmp/greeter`;
 
-const colors = JSON.parse(exec(`matugen image '${State.wallpaper}' --json hex`))
-  .colors.light;
+const colors = await execAsync(`matugen image '${State.wallpaper}' --json hex`)
+  .then(JSON.parse)
+  .catch((e) => {
+    printerr("Matugen failed:", e);
+    return colors_fallback;
+  })
+  .then((v) => v.colors.light);
 
 const colors_scss = Object.entries(colors).reduce(
   (acc, [key, value]) => `$${key}: ${value}; ${acc}`,
   `$font_family: "${State.font_family}"; $wallpaper: "file://${State.wallpaper}"`,
 );
 
-exec([
+await execAsync([
   "bash",
   "-c",
   `mkdir -p /tmp/greeter; printf '${colors_scss}' >${dynamic_path}/dynamic.scss`,
 ]);
 
-export default exec(
+export default await execAsync(
   `sass ${static_path}/index.scss --no-source-map --load-path=${static_path} --load-path=${dynamic_path}`,
-);
+).catch((e) => {
+  printerr("Sass failed:", e);
+  return "";
+});
