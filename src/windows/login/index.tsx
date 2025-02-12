f54f7a93adeab81fe11f65f74dc650fa2430c487
h-banii <121690516+h-banii@users.noreply.github.com>
2025-02-11 22:53:04 -0300

    style: Add glow behind login card

--- src/windows/login/index.tsx
+++ src/windows/login/index.tsx
@@ -59,49 +59,51 @@ export default function Login(
       gdkmonitor={gdkmonitor}
       exclusivity={Astal.Exclusivity.IGNORE}
       application={App}
+      halign={Gtk.Align.CENTER}
+      valign={Gtk.Align.CENTER}
     >
-      <box
-        cssClasses={["gradient"]}
+      <Frame
+        cssClasses={["radial-gradient"]}
         halign={Gtk.Align.CENTER}
         valign={Gtk.Align.CENTER}
       >
-        <Frame
-          type="overlay"
-          halign={Gtk.Align.CENTER}
-          vexpand
-          cssClasses={["login-container"]}
-        >
-          <box
-            spacing={7}
-            orientation={Gtk.Orientation.VERTICAL}
+        <Frame cssClasses={["gradient"]}>
+          <Frame
             halign={Gtk.Align.START}
-            valign={Gtk.Align.CENTER}
             vexpand
+            cssClasses={["login-container"]}
           >
-            <label
-              cssClasses={["logo"]}
-              halign={Gtk.Align.CENTER}
-              // valign={Gtk.Align.END}
+            <box
+              spacing={7}
+              orientation={Gtk.Orientation.VERTICAL}
+              halign={Gtk.Align.START}
+              valign={Gtk.Align.CENTER}
+              vexpand
             >
-              {State.vendor_name}
-            </label>
-            <entry
-              placeholderText="username"
-              onActivate={(self) => (username = self.text)}
-            />
-            <PasswordEntry
-              placeholderText="password"
-              onActivate={(self) => {
-                password = self.text;
-                login();
-              }}
-            />
-            <button cssClasses={["login-button"]} onClicked={login}>
-              login
-            </button>
-          </box>
+              <label
+                cssClasses={["logo"]}
+                halign={Gtk.Align.CENTER}
+              >
+                {State.vendor_name}
+              </label>
+              <entry
+                placeholderText="username"
+                onActivate={(self) => (username = self.text)}
+              />
+              <PasswordEntry
+                placeholderText="password"
+                onActivate={(self) => {
+                  password = self.text;
+                  login();
+                }}
+              />
+              <button cssClasses={["login-button"]} onClicked={login}>
+                login
+              </button>
+            </box>
+          </Frame>
         </Frame>
-      </box>
+      </Frame>
     </window>
   );
 }
